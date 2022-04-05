fa935ee89a115452a02e56f6cd0079f51e547864
GabrielaMendozal <mendozalizethg@gmail.com>
2022-04-04 19:54:47 -0500

    formulario con hooks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import Form from './Componentes/Form/Form';
+import Results from './Componentes/Form/Results';
 
 function App() {
+ 
+  const [state, setState ]= useState ({
+    firstName: "",
+    lastName : "",
+    email : "",
+    password : "",
+    confirmPassword: ""
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+        <Form inputs ={state} setInputs={setState}/>
+        <Results data={state} />
     </div>
   );
 }
+
 
 export default App;
